d37bace36a90c16497070366eaafffcb0c8fd89d
Dmitry Matin <super.dmitry320@yandex.ru>
2022-02-10 22:41:02 +0300

    Code refactoring

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
         a = float(input('Input a: '))
         b = float(input('Input b: '))
         c = float(input('Input c: '))
+        
         x_start = int(input('Input the start value of x: '))
         x_end = int(input('Input the end value of x: '))
         dx = int(input('Input dx: '))
