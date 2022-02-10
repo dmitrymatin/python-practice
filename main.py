de4abdc50fab5a8288cce9caf8c7092398041365
Dmitry Matin <super.dmitry320@yandex.ru>
2022-02-10 22:35:52 +0300

    Modify validation methods to raise exceptions

--- main.py
+++ main.py
@@ -52,23 +52,17 @@ def f(a, b, c, x):
 
 def validate_first_case(a, b, x):
     if 10 + b == 0:
-        return False
-
-    return True
+        raise ArithmeticError('First case is invalid')
 
 
 def validate_second_case(a, c, x):
     if x - c == 0:
-        return False
-
-    return True
+        raise ArithmeticError('Second case is invalid')
 
 
 def validate_third_case(x, c):
     if c == 0:
-        return False
-
-    return True
+        raise ArithmeticError('Third case is invalid')
 
 
 def validate_all_numeric(*argv):
