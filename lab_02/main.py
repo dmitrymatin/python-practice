cfccb05c20b5ca29b94944b92eecc3a349c792e7
Dmitry Matin <super.dmitry320@yandex.ru>
2022-02-13 13:59:31 +0300

    add second version for lab_02

--- lab_02/main.py
+++ lab_02/main.py
@@ -15,13 +15,14 @@ def main():
     position = listSize - 1
     for i in reversed(range(0, len(list))):
         if list[i] <= 0:
-            j = i
-            while (j != position):
-                j += 1
-            if i != j and j - 1 >= 0: # if we actually moved in while loop and if the previous index is valid
-                tmp = list[j]
-                list[j] = list[j - 1]
-                list[j - 1] = tmp
+            j = position
+            # while (j != position):
+            #     j += 1
+            if i != j and j - 2 >= 0: # if we actually moved in while loop and if the previous index is valid
+                tmp = list[j - 1]
+                list[j - 1] = list[i]
+                del list[i]
+                list[j - 2] = tmp
                 position = j - 1
 
     for i in list:
