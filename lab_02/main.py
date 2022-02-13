c9a4f5341bc2150118f315649c9b9e6663b3eaee
Dmitry Matin <super.dmitry320@yandex.ru>
2022-02-13 13:44:23 +0300

    add initial code for lab_02

--- lab_02/main.py
+++ lab_02/main.py
@@ -10,6 +10,23 @@ def main():
     for i in list:
         print(i, end = ' ')
 
+    print()
+
+    position = listSize - 1
+    for i in reversed(range(0, len(list))):
+        if list[i] <= 0:
+            j = i
+            while (j != position):
+                j += 1
+            if i != j and j - 1 >= 0: # if we actually moved in while loop and if the previous index is valid
+                tmp = list[j]
+                list[j] = list[j - 1]
+                list[j - 1] = tmp
+                position = j - 1
+
+    for i in list:
+        print(i, end = ' ')
+
 
 if __name__ == "__main__":
     main()
