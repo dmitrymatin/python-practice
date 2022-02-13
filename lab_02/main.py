424c81c82449e01dc8711f421925afd1a254a2ba
Dmitry Matin <super.dmitry320@yandex.ru>
2022-02-13 13:09:24 +0300

    Add initial code for initialising array

--- lab_02/main.py
+++ lab_02/main.py
@@ -1,5 +1,18 @@
+from random import randrange
+
+
 def main():
-    return
+    intervalMinimun = -100
+    intervalMaximum = 100
+    listSize = 10
+    list = []
+    for i in range(0, listSize):
+        random_value = randrange(intervalMinimun, intervalMaximum + 1)
+        list.append(random_value)
+
+    for i in list:
+        print(i, end = ' ')
+
 
 if __name__ == "__main__":
     main()
