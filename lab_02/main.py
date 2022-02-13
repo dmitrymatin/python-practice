a66da1b95c956ae46ce0400c5b864d393fdeb345
Dmitry Matin <super.dmitry320@yandex.ru>
2022-02-13 13:12:23 +0300

    Use list comprehensions for list initialisation

--- lab_02/main.py
+++ lab_02/main.py
@@ -5,10 +5,7 @@ def main():
     intervalMinimun = -100
     intervalMaximum = 100
     listSize = 10
-    list = []
-    for i in range(0, listSize):
-        random_value = randrange(intervalMinimun, intervalMaximum + 1)
-        list.append(random_value)
+    list = [randrange(intervalMinimun, intervalMaximum + 1) for _ in range(listSize)]
 
     for i in list:
         print(i, end = ' ')
