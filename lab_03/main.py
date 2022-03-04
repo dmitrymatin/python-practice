d4319291653440fb14c82956260bf746446b6db1
Dmitry Matin <super.dmitry320@yandex.ru>
2022-03-04 08:30:39 +0300

    Add test 2 for lab 03

--- lab_03/main.py
+++ lab_03/main.py
@@ -24,13 +24,13 @@ def main():
             "surname": "Иванов",
             "name": "Петр",
             "age": 20,
-            "conscription": 0,
+            "conscription": 1,
         },
         3: {
             "surname": "Кузнецов",
             "name": "Сергей",
             "age": 21,
-            "conscription": 0,
+            "conscription": 1,
         },
         4: {
             "surname": "Соколов",
@@ -48,7 +48,7 @@ def main():
             "surname": "Лебедев",
             "name": "Иван",
             "age": 18,
-            "conscription": 0,
+            "conscription": 1,
         },
         7: {
             "surname": "Козлов",
@@ -174,6 +174,7 @@ def main():
 
             # Фамилии людей среди невоеннообязанных и их разница в возрасте
             print(f"\tEmployee {employee['surname']} and {matchEmployee['surname']} have an age difference {abs(employee['age'] - matchEmployee['age'])}")
+
 
 
 if __name__ == "__main__":
