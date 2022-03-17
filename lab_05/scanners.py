9af955b175e219e5ae703679b53081e7ed02f974
Dmitry Matin <super.dmitry320@yandex.ru>
2022-03-17 22:36:50 +0300

    Add initial data generation for lab 05

--- lab_05/scanners.py
+++ lab_05/scanners.py
@@ -1,5 +1,22 @@
 def main():
-    pass
+    num_scanners = 4
+    scanners = []
+    for i in range(num_scanners):
+        scanners.append(
+            {
+                "name": f"scanner{i * 10}",
+                "price": (i ^ 1) << 1,
+                "horizontal_surface_scan_size": 297,
+                "vertical_surface_scan_size": 216,
+                "optical_horizontal_resolution": 600,
+                "optical_vertical_resolution": 600,
+                "greyscale": 1 << 8,  # 256 bit
+            }
+        )
+
+    for s in scanners:
+        print(s)
+
 
 if __name__ == "__main__":
     main()
