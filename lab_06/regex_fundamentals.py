b458ca83e6a7bab1478d61ac06dc30f10d5faf50
Dmitry Matin <super.dmitry320@yandex.ru>
2022-03-26 00:17:52 +0300

    add another regex example from python docs

--- lab_06/regex_fundamentals.py
+++ lab_06/regex_fundamentals.py
@@ -6,6 +6,10 @@ def main():
     m = p.findall("#B63")
     print(m)
 
+    p = re.compile(r'\d+')
+    m = p.findall('12 drummers drumming, 11 pipers piping, 10 lords a-leaping')
+    print(m)
+
 
 if __name__ == "__main__":
     main()
