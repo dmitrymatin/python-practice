b21c8cca95c13f745eb467f09c7dabecdd9c541b
Dmitry Matin <super.dmitry320@yandex.ru>
2022-04-01 09:21:08 +0300

    complete lab_06

--- lab_06/regex.py
+++ lab_06/regex.py
@@ -5,22 +5,22 @@ def main():
     contents = ""
     with open("sample_file.txt", "r") as file:
         contents = file.read(1000000)
-        print(len(contents))
-    
-    test = "XYZXYZXYXXXYYXXYZXXYXZYX"
-
+        
     pattern = re.compile(r"((XYZ)+(XYZ|XY|X))", re.M)
     results = pattern.findall(contents)
 
 
     max_match = 0
+    max_match_value = ""
     for res in results:
         for match in res:
             match_length = len(match)
             if match_length > max_match:
                 max_match = match_length
+                max_match_value = match
 
-    print(max_match)
+    print(f'longest match: {max_match}')
+    print(f'the match is: {max_match_value}')
 
 
 if __name__ == "__main__":
